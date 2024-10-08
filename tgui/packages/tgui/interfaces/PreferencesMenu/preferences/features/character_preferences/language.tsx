5e0dab8b4d19003c9cf0b5b6c41934080fd44a88
Yische <126526797+merawera@users.noreply.github.com>
2024-10-08 04:46:52 +0100

    Delete tgui directory

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/language.tsx
+++
@@ -1,7 +0,0 @@
-import { FeatureChoiced } from '../base';
-import { FeatureDropdownInput } from '../dropdowns';
-
-export const language: FeatureChoiced = {
-  name: 'Language',
-  component: FeatureDropdownInput,
-};
